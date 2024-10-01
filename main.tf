6217ca198eb8052e7b9256da2fb5e14a9848149d
Wanderson <wanderson.p.ayres@gmail.com>
2024-09-30 21:56:48 -0300

    Update main.tf
    Limpeza do código e documentação.

--- main.tf
+++ main.tf
@@ -25,14 +25,11 @@ resource "aws_db_instance" "db_instance" {
 module "vpc" {
 
   source = "terraform-aws-modules/vpc/aws" 
-
   name = "vpc-databases" 
   cidr = "10.0.0.0/16" 
-
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -50,7 +47,6 @@ resource "aws_subnet" "database_subnet_az1" {
   vpc_id            = module.vpc.vpc_id
   cidr_block        = "10.0.5.0/24"
   availability_zone = "us-east-1a"
-
   tags = {
     Name = "food_database_subnet_az_1"
   }
@@ -60,7 +56,6 @@ resource "aws_subnet" "database_subnet_az2" {
   vpc_id            = module.vpc.vpc_id
   cidr_block        = "10.0.6.0/24"
   availability_zone = "us-east-1b"
-
   tags = {
     Name = "food_database_subnet_az_2"
   }
@@ -70,7 +65,6 @@ resource "aws_subnet" "database_subnet_az2" {
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
   subnet_ids =  module.vpc.private_subnets
-
   tags = {
     Name = "db_subnet_group"
   }
@@ -80,19 +74,16 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 resource "aws_dynamodb_table" "dynamo_db" {
   name           = "fiap-self-service-pedidos-ativos"
   billing_mode   = "PAY_PER_REQUEST"
-
   hash_key       = "id"         # Definindo "id" como chave primária (hash key)
   range_key      = "created_at" # Definindo "created_at" como chave de classificação (range key)
   attribute {
     name = "id"
     type = "S" # Tipo String
   }
-
   attribute {
     name = "created_at"
     type = "S" # Tipo String para armazenar datas
   }
-
   tags = {
     Name = "fiap-self-service-pedidos-ativos"
   }
@@ -103,21 +94,18 @@ resource "aws_security_group" "rds_sg" {
   name        = "rds_security_group"
   description = "Security group para o RDS e DocumentDB"
   vpc_id      = module.vpc.vpc_id
-
   ingress {
     from_port   = 3306 # Porta MySQL (RDS)
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Defina um IP específico para acesso controlado
   }
-
   ingress {
     from_port   = 27017 # Porta MongoDB (DocumentDB)
     to_port     = 27017
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
