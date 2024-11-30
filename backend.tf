eda9db6636ef3c02f84e579e8f2bf34bdc4ab4eb
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-30 08:28:57 -0300

    Deploy fase4

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-unique-fiap-database"
+    bucket         = "terraform-state-bucket-unique-fiap-fase4-database"
     key            = "terraform/state"
     region         = "us-east-1"
   }
